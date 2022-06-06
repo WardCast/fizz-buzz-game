59c770a593346e939ffe30e5c463b22f87e2e236
WardCast <killianward@gmail.com>
2022-06-06 17:35:37 +0100

    minor refactoring

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def check_if_correct(user_input, correct_answer):
+def is_correct(user_input, correct_answer):
     if user_input.lower() == correct_answer:
         return True
     elif str(correct_answer) == user_input:
@@ -17,36 +17,35 @@ def get_correct_answer(fizz_num, buzz_num, current_num):
     else:
         return current_num
 
+# Setting preferences
+fizz_num = input("\nEnter what you want fizz to be > ")
+buzz_num = input("Enter what you want buzz to be >  ")
+lives = input("Enter number the number of lives you want (Options: |1| |2| |3|) > ")
 
-fizz_num = input("Enter what you want fizz to be: ")
-buzz_num = input("Enter what you want buzz to be: ")
-starting_num_of_lives = int(input("Enter number of lives. Options: |1| |2| |3|\n"))
-valid_values = [1, 2, 3]
-
-while starting_num_of_lives not in valid_values:
-    starting_num_of_lives = int(input("Please enter a valid value: "))
+while lives not in "123":
+    lives = input("Please enter a valid value: ")
 
 # Rules
-print(f"""If the number is {fizz_num} or is a multiple of {fizz_num}, enter \"Fizz\"
+print(f"""\nIf the number is {fizz_num} or is a multiple of {fizz_num}, enter \"Fizz\"
 If the number is {buzz_num} or is a multiple of {buzz_num}, enter \"Buzz\"
 If the number is a multiple of both {fizz_num} and {buzz_num}, enter \"FizzBuzz\"
 If the number is none of the above, enter the current number""")
 
-current_num_of_lives = starting_num_of_lives
+current_lives = int(lives)
 alive = True
 current_num = 1
 while alive:
     print("")
     print(current_num)
-    user_input = input("Enter your answer: ")
+    user_answer = input("Enter your answer >  ")
     correct_answer = get_correct_answer(int(fizz_num), int(buzz_num), current_num)
-    is_correct = check_if_correct(user_input, correct_answer)
-    if is_correct:
+    
+    if is_correct(user_answer, correct_answer):
         current_num += 1
     else:
-        current_num_of_lives -= 1
-        print(f"Lives left: {current_num_of_lives}/{starting_num_of_lives}")
-        if current_num_of_lives < 1:
-            print("You lost - (insert unhelpfully sympathetic comment)")
+        current_lives -= 1
+        print(f"\nLives left: {current_lives}/{lives}")
+        if current_lives < 1:
+            print("You lost - (insert unhelpfully sympathetic comment)\n")
             alive = False
 
